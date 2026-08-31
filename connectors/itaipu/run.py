1a9825ffe5e06ed42b7549cb89bf59cd85be943c
garraguarani <retratoai@gmail.com>
2026-08-30 22:44:01 -0300

    datos: itaipu-indicadores.json generado desde ONS

--- connectors/itaipu/run.py
+++ connectors/itaipu/run.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 if _root not in sys.path:
     sys.path.insert(0, _root)
 
