3318d777b23776d62607ba80d265be6fa5565216
garraguarani <retratoai@gmail.com>
2026-09-02 02:44:20 -0300

    fix(yacyreta): curados con nombres consistentes (generacion_total/suministro_*) para pagina

--- connectors/yacyreta/curados.py
+++ connectors/yacyreta/curados.py
@@ -3,6 +3,7 @@ URL_YACYRETA = "https://paraguayoindependiente.com/2026/01/21/yacyreta-muestra-u
 def _r(ind, val, uni, periodo, fuente, url):
     return metadata.build_record(ind, val, uni, periodo, fuente=fuente, url=url, metodo="extraccion_manual", estado="verificado")
 CURADOS = [
-    _r("generacion_yacyreta_paraguay", 3081.0, "GWh", 2025, "Entidad Binacional Yacyretá", URL_YACYRETA),
-    _r("generacion_yacyreta_total", 16103.0, "GWh", 2025, "Entidad Binacional Yacyretá", URL_YACYRETA),
+    _r("generacion_total", 16103.0, "GWh", 2025, "Entidad Binacional Yacyretá", URL_YACYRETA),
+    _r("suministro_paraguay", 3081.0, "GWh", 2025, "Entidad Binacional Yacyretá", URL_YACYRETA),
+    _r("suministro_argentina", 13022.0, "GWh", 2025, "Entidad Binacional Yacyretá", URL_YACYRETA),
 ]
