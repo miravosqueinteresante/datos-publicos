484feec97f5e9e2d8f5c8438f71aaea50613235d
garraguarani <retratoai@gmail.com>
2026-09-02 19:34:17 -0300

    feat(yacyreta): agregar 2016 record histórico 21.627 GWh (EBY via ABC)

--- connectors/yacyreta/curados.py
+++ connectors/yacyreta/curados.py
@@ -3,8 +3,9 @@ URL_YACYRETA = "https://paraguayoindependiente.com/2026/01/21/yacyreta-muestra-u
 def _r(ind, val, uni, periodo, fuente, url):
     return metadata.build_record(ind, val, uni, periodo, fuente=fuente, url=url, metodo="extraccion_manual", estado="verificado")
 URL_EBY_2019 = "https://www.eby.gov.py/informe-de-produccion-anual-de-la-central-hidroelectrica-yacyreta/"
-URL_EBY_2023 = "https://www.eby.gov.py/datos-oficiales-sobre-generacion-de-yacyreta-en-diciembre-2023/"
+URL_ABC_2016 = "https://www.abc.com.py/edicion-impresa/suplementos/economico/2024/02/25/yacyreta-paraguay-uso-7-y-argentina-93/"
 CURADOS = [
+    _r("generacion_total", 21627.0, "GWh", 2016, "EBY", URL_ABC_2016),
     _r("generacion_total", 17281.0, "GWh", 2019, "EBY", URL_EBY_2019),
     _r("generacion_total", 16103.0, "GWh", 2025, "Entidad Binacional Yacyretá", URL_YACYRETA),
     _r("suministro_paraguay", 3081.0, "GWh", 2025, "Entidad Binacional Yacyretá", URL_YACYRETA),
